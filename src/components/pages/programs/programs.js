ff92c414c049382ee83df23aab9d4434a615620c
AlexHanchak <ohanchak@gmail.com>
2020-11-13 15:51:02 +0200

    alfa

--- src/components/pages/programs/programs.js
+++ src/components/pages/programs/programs.js
@@ -12,38 +12,43 @@ const Programs = () => {
         </p>
         <form>
           <div className="row" id="topForm">
-            <div className="col">
-              <label>
-                ИМЯ:*
-                <input id="name" placeholder="Barsick" />
-              </label>
-              <label>
-                ВЕС:(КГ)*
+            <div className="col" id="tfl">
+              <div>
+                <label for="name">
+                  ИМЯ:*
+                  <input id="name" placeholder="Barsick" />
+                </label>
+              </div>
+              <div>
+                <label for="weight">ВЕС:(КГ)*</label>
                 <input id="weight" placeholder="7" />
-              </label>
-              <label>
-                ВОЗРАСТ:(ЛЕТ)*
+              </div>
+              <div>
+                <label for="age">ВОЗРАСТ:(ЛЕТ)*</label>
                 <input id="age" placeholder="7" />
-              </label>
+              </div>
             </div>
-            <div>
-              <label>
-                <input type="radio" checked="true" />
-                ПОХУДЕНИЕ
-              </label>
-              <label>
-                <input type="radio" />
-                НАБОР МАССЫ
-              </label>
-              <label>
-                <input type="radio" />
-                НЕ ЗНАЮ(НУЖЕН ВАШ СОВЕТ)
-              </label>
+            <div className="col" id="tfr">
+              <div className="col" id="tfrRadio">
+                <label>
+                  <input type="radio" />
+                  ПОХУДЕНИЕ
+                </label>
+                <label>
+                  <input type="radio" />
+                  НАБОР МАССЫ
+                </label>
+                <label>
+                  <input type="radio" />
+                  НЕ ЗНАЮ(НУЖЕН ВАШ СОВЕТ)
+                </label>
+              </div>
             </div>
           </div>
+          <hr/>
           <div id="cData">
             <h3>Контактные данные(Владельца кота)</h3>
-            <div>
+            <div id="conactD">
               <div>
                 <label>
                   E-MAIL:*
@@ -58,10 +63,12 @@ const Programs = () => {
               </div>
             </div>
           </div>
+          <hr/>
           <div id="comm">
             <h3>Коментарий</h3>
             <textarea placeholder="РАССКАЖИТЕ ОБО ВСЕХ ПОВАДКАХ КОТА" />
           </div>
+          <hr/>
           <div id="more">
             <h3>Дополнительно</h3>
             <label>
